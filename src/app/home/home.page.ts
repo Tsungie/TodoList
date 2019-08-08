8ab39d3418edd23c92c9de57f426d887cc8098e2
Tsungie <misstsungie@gmail.com>
2019-08-08 03:45:35 +0200

    add, edit and delete

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  taskList = [];
+
+
+  constructor(public navCtrl: NavController, public alertController: AlertController) {}
+
+
+  addTask() {
+    if (this.taskName.length > 0) {
+        let task = this.taskName;
+        this.taskList.push(task);
+        this.taskName = "";
+    }
+}
+
+deleteTask(index){
+  this.taskList.splice(index, 1);
+}
+
+
+  async updateTask(index) {
+  const alert = await this.alertController.create({
+      header: 'Update Task?',
+      message: 'Type in your new task to update.',
+      inputs: [{ name: 'editTask', placeholder: 'Task' }],
+      buttons: [{ text: 'Cancel', role: 'cancel' },
+                { text: 'Update', handler: data => {
+                    this.taskList[index] = data.editTask; }
+                }
+               ]
+  });
+  alert.present();
+}
+}
